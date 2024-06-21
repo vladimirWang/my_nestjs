a2001b56e236fc05c818af9767d0017dd639ae25
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-06-21 09:13:08 +0800

    logger工具

--- src/@nestjs/core/logger.ts
+++ src/@nestjs/core/logger.ts
@@ -1,18 +1,22 @@
 import clc from "cli-color";
 
 export class Logger {
+  private static lastLogTime = Date.now();
   static log(message: string, context: string = "") {
     // 获取当前时间戳
     const timestamp = new Date().toLocaleString();
 
     // 获取当前进程id
     const pid = process.pid;
+    const currentTime = Date.now();
+    const timeDiff = currentTime - this.lastLogTime;
     console.log(
       `[${clc.green("Nest")}] ${clc(
-        pid.toString
+        pid.toString()
       )}  - ${timestamp}     ${clc.green("LOG")} [${clc.yellow(
         context
-      )}] ${clc.green(message)}`
+      )}] ${clc.green(message)} +${clc.green(timeDiff)}ms`
     );
+    this.lastLogTime = currentTime;
   }
 }
